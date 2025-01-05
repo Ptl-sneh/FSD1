278b27f9c2691b7f83b3821692e0c1b947123411
Ptl-sneh <snehpatel641@gmail.com>
2025-01-05 09:34:24 +0530

    Happy Number

---
+++ javascript/happynumber.js
@@ -0,0 +1,26 @@
+var num;
+function fun() {
+    num = prompt("Enter a number: ");
+    var result = happynumber(num);
+    
+    if (result) {
+        document.getElementById("ans").innerHTML = "It is a happy number"
+    } else {
+        document.getElementById("ans").innerHTML = "It is not a happy number"
+    }
+}
+
+function happynumber(n1) {
+    var sum = 0;
+    while (n1 > 0) {
+        r = n1 % 10;
+        sum = sum + r * r;
+        n1 = Math.floor(n1 / 10);
+    }
+    if (sum == 1) {
+        return true;
+    } else if (sum > 1 && sum <= 4) {
+        return false;
+    }
+    return happynumber(sum);
+}
